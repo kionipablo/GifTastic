b173287ec99383d12c0dffad46a8a4879b7454b5
Kioni Pablo <kionipablo@gmail.com>
2019-09-29 16:43:18 -0700

    Adding updated code

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,71 +1,77 @@
-var topics = ["J Cole", "Drake", "Ed Sheeran", "Beyonce", "Marshmello", "Jay Z", "Eminem", "Rihanna"];
+$(document).ready(function(){
+    var topics = ["J Cole", "Drake", "Ed Sheeran", "Beyonce", "Marshmello", "Jay Z", "Eminem", "Rihanna"];
+    console.log(topics);
 
 
 
-//Function to create buttons
-function createButtons(){
+    //Function to create buttons
+    function createButtons(){
 
-    //To empty giphy button to avoid repeat buttons
-    $("#giphy-button").empty();
+        //To empty giphy button to avoid repeat buttons
+        $("#giphy-button").empty();
 
-    //For loop used to go through topics array and then generate a button for each artist
-    for (var i = 0; i < topics.length; i++){
-        var newButton = $('<button/>');
-        newButton.attr("data",topics[i]);
-        newButton.addClass("button");
-        newButton.text(topics[i]);
-        $("#giphy-button").append(newButton);
+        //For loop used to go through topics array and then generate a button for each artist
+        for (var i = 0; i < topics.length; i++){
+
+            var newButton = $("<button/>");
+            newButton.attr("data-name",topics[i]);
+            newButton.addClass("artist-button");
+            newButton.text(topics[i]);
+
+            $("#giphy-button").append(newButton);
+        };
     };
-};
 
-createButtons();
+    $("#submit").on("click", function(event) {
 
+        //Prevents submit botton from trying to send a form
+        event.preventDefault();
 
-$("#submit").on("click", function() {
+        //Taking the user input in the search box and assigning it a variable and push it into the topics array
+        var userSearch = $("#add-artist").val().trim();
 
-    //Prevents submit botton from trying to send a form
-    event.preventDefault();
+        if (userSearch === ""){
 
-    //Taking the user input in the search box and assigning it a variable and push it into the topics array
-    var userSearch = $("#add-artist").val();
-
-    if (userSearch === ""){
-
-    }else{
-        topics.push(userSearch);
-    }
+        }else{
+            topics.push(userSearch);
+        }
+        createButtons();
+        console.log(topics);
+    });
 
     createButtons();
-});
 
-$("button").on("click",function(){
-    
-    var userSearch = $(this).attr("data");
-    var APIKey = "&api_key=Cs3xYChN25pFn0Gy09deyv1OzTZtGOj1&limit=10"
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userSearch + APIKey ;
+    $(document).on("click",".artist-button",function(){
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-          console.log(response);
-        var results = response.data;
+        $("#giphy-images").empty();
+        var person = $(this).attr("data-name");
+        console.log(person);
+        var APIKey = "&api_key=Cs3xYChN25pFn0Gy09deyv1OzTZtGOj1&limit=10"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + APIKey ;
 
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div>");
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(response) {
+            console.log(response);
+            var results = response.data;
 
-          var rating = results[i].rating;
+            for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
 
-          var p = $("<p>").text("Rating: " + rating);
+            var rating = results[i].rating;
 
-          var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
+            var p = $("<p>").text("Rating: " + rating);
 
-          gifDiv.append(p);
-          gifDiv.append(personImage);
+            var personImage = $("<img>").addClass("giphs");
+            personImage.attr("src", results[i].images.fixed_height.url);
 
-          $("#giphy-images").prepend(gifDiv);
-        }
-      });
+            gifDiv.append(p);
+            gifDiv.append(personImage);
+
+            $("#giphy-images").prepend(gifDiv);
+            }
+        });
+    });
 });
